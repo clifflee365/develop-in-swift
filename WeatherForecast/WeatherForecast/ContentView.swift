1ffacafa5dd554ac82f0442376b0e049fb9843f2
cliff <clifflee1022@gmail.com>
2024-04-04 16:01:10 +0800

    ch2 init: Views, structures, and properties

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by lifeng on 2024/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by lifeng on 2024/4/4.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
