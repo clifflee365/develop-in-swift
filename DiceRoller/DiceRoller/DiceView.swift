7c90e51989d282f2d507c789f7c32e9ee7e9b6f1
cliff <clifflee1022@gmail.com>
2024-04-05 17:26:25 +0800

    ch4: buttons and state  ok

--- DiceRoller/DiceRoller/DiceView.swift
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
     var body: some View {
-        Image(systemName: "die.face.1")
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 100)
+            .aspectRatio(1, contentMode: .fit)
+            .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
     }
 }
 
