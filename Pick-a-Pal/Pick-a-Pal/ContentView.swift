53e37f83f48575e204a73234b80552c295a385e5
cliff <clifflee1022@gmail.com>
2024-04-05 22:04:16 +0800

    ch5: init - Lists and text fields

---
+++ Pick-a-Pal/Pick-a-Pal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by lifeng on 2024/4/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by lifeng on 2024/4/5.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
